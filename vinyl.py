28cc019264632c8823da2ab58b5d1d04df40ed1b
Jayson <contact@jaysonmorberg.com>
2021-09-26 16:00:17 -0700

    wrapped the main bot functions into a class

--- vinyl.py
+++ vinyl.py
@@ -3,14 +3,10 @@ import os
 import discord
 import youtube_dl
 from dotenv import load_dotenv
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-# Set discord intents to all for now
-intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='-', intents=intents)
 
 # Suppress noise about consule usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
@@ -31,10 +27,10 @@ YTDL_FORMAT_OPTIONS = {
 }
 
 FFMPEG_OPTIONS = {
-    'options': '-vn'
+    'options': '-vn',
 }
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+ytdl = youtube_dl.YoutubeDL(YTDL_FORMAT_OPTIONS)
 
 # Class to download youtube audio and return filenames
 class YTDLSource(discord.PCMVolumeTransformer):
@@ -54,72 +50,83 @@ class YTDLSource(discord.PCMVolumeTransformer):
         filename = data['title'] if stream else ytdl.prepare_filename(data)
         return filename, data['title']
 
-@bot.command(name='join', aliases=['j'], help='Ask Vinyl to join the voice channel')
-async def join(ctx):
-    if not ctx.message.author.voice:
-        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
-        return
-    else:
-        channel = ctx.message.author.voice.channel
-    await channel.connect()
+# Bot class for commands
+class Vinyl(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-@bot.command(name='leave', aliases=['l'], help='Make Vinyl leave the voice channel')
-async def leave(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client is not None:
-        await voice_client.disconnect()
-    else:
-        await ctx.send("Vinyl is not connected to a voice channel")
+    @commands.command(name='join', aliases=['j'], help='Ask Vinyl to join the voice channel')
+    async def _join(self, ctx):
+        if not ctx.message.author.voice:
+            await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+            return
+        else:
+            channel = ctx.message.author.voice.channel
+        await channel.connect()
 
-@bot.command(name='play', aliases=['p'], help="Play audio from YouTube")
-async def play(ctx,url):
-    try:
-        server = ctx.message.guild
-        voice_client = server.voice_client
+    @commands.command(name='leave', aliases=['l'], help='Make Vinyl leave the voice channel')
+    async def _leave(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client is not None:
+            await voice_client.disconnect()
+        else:
+            await ctx.send("Vinyl is not connected to a voice channel")
 
-        async with ctx.typing():
-            file, title = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_client.play(discord.FFmpegPCMAudio(source=file))
+    @commands.command(name='play', aliases=['p'], help="Play audio from YouTube")
+    async def _play(self, ctx, url):
+        try:
+            server = ctx.message.guild
+            voice_client = server.voice_client
+
+            async with ctx.typing():
+                file, title = await YTDLSource.from_url(url, loop=bot.loop)
+                voice_client.play(discord.FFmpegPCMAudio(source=file, **FFMPEG_OPTIONS))
             
         
-        await ctx.send('**Now Playing:** {}'.format(title))
-        os.remove(file)
+            await ctx.send('**Now Playing:** {}'.format(title))
+            os.remove(file)
     
-    except:
-        await ctx.send("Vinyl is not connected to a voice channel currently")
+        except:
+            await ctx.send("Vinyl is not connected to a voice channel currently")
 
-@bot.command(name='pause', help='Pause any song Vinyl is currently playing')
-async def pause(ctx):
-    voice_client = ctx.message.guild.voice_client
 
-    if voice_client is None:
-        await ctx.sent("Vinyl is not connected to a voice channel currently")
-    elif voice_client.is_playing():
-        voice_client.pause()
-    else:
-        await ctx.send("Vinyl isn't playing anything")
+    @commands.command(name='pause', help='Pause any song Vinyl is currently playing')
+    async def _pause(self, ctx):
+        voice_client = ctx.message.guild.voice_client
 
-@bot.command(name='resume', help='Resume a paused song')
-async def resume(ctx):
-    voice_client = ctx.message.guild.voice_client
-    
-    if voice_client is None:
-        await ctx.sent("Vinyl is not connected to a voice channel currently")
-    elif voice_client.is_paused():
-        voice_client.resume()
-    else:
-        await ctx.send("Vinyl isn't paused currently")
+        if voice_client is None:
+            await ctx.sent("Vinyl is not connected to a voice channel currently")
+        elif voice_client.is_playing():
+            voice_client.pause()
+        else:
+            await ctx.send("Vinyl isn't playing anything")
 
-@bot.command(name='stop', aliases=['s'], help='Stop any song Vinyl is currently playing')
-async def stop(ctx):
-    voice_client = ctx.message.guild.voice_client
-    
-    if voice_client is None:
-        await ctx.sent("Vinyl is not connected to a voice channel currently")
-    elif voice_client.is_playing():
-        voice_client.stop()
-    else:
-        await ctx.send("Vinyl isn't playing anything")
+    @commands.command(name='resume', help='Resume a paused song')
+    async def _resume(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        
+        if voice_client is None:
+            await ctx.sent("Vinyl is not connected to a voice channel currently")
+        elif voice_client.is_paused():
+            voice_client.resume()
+        else:
+            await ctx.send("Vinyl isn't paused currently")
+
+    @commands.command(name='stop', aliases=['s'], help='Stop any song Vinyl is currently playing')
+    async def _stop(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        
+        if voice_client is None:
+            await ctx.sent("Vinyl is not connected to a voice channel currently")
+        elif voice_client.is_playing():
+            voice_client.stop()
+        else:
+            await ctx.send("Vinyl isn't playing anything")
+
+# Set discord intents to all for now
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='-', intents=intents)
+bot.add_cog(Vinyl(bot))
 
 @bot.event
 async def on_ready():
