d96f5e7a2837ac1671b17f0b65acac28fb60535c
Jayson <contact@jaysonmorberg.com>
2021-11-15 02:50:58 -0800

    switched to a fork of youtube_dl that disables download throttling and renamed/reorganized some variables

--- vinyl.py
+++ vinyl.py
@@ -1,39 +1,41 @@
 # vinyl.py
 import os
 import discord
-import youtube_dl
+import yt_dlp
+import asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Suppress noise about consule usage from errors
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-YTDL_FORMAT_OPTIONS = {
-    'format': 'bestaudio/best',
-    'extractaudio': True,
-    'audioformat': 'mp3',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' 
-}
-
 FFMPEG_OPTIONS = {
     'options': '-vn',
 }
 
-ytdl = youtube_dl.YoutubeDL(YTDL_FORMAT_OPTIONS)
+# Suppress unnecessary bug reports
+yt_dlp.utils.bug_reports_message = lambda: ''
 
 # Class to download youtube audio and return filenames
-class YTDLSource(discord.PCMVolumeTransformer):
+class AudioSource(discord.PCMVolumeTransformer):
+
+    YTDL_FORMAT_OPTIONS = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioformat': 'mp3',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0' 
+    }
+
+    ytdl = yt_dlp.YoutubeDL(YTDL_FORMAT_OPTIONS)
+
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
         self.data = data
@@ -43,11 +45,11 @@ class YTDLSource(discord.PCMVolumeTransformer):
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        data = await loop.run_in_executor(None, lambda: cls.ytdl.extract_info(url, download=not stream))
         if 'entries' in data:
             # take first item from a playlist
             data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        filename = data['title'] if stream else cls.ytdl.prepare_filename(data)
         return filename, data['title']
 
 # Bot class for commands
@@ -76,20 +78,20 @@ class Vinyl(commands.Cog):
 
     @commands.command(name='play', aliases=['p'], help="Play audio from YouTube")
     async def _play(self, ctx, url):
-        try:
-            server = ctx.message.guild
-            voice_client = server.voice_client
+        #try:
+        server = ctx.message.guild
+        voice_client = server.voice_client
 
-            async with ctx.typing():
-                file, title = await YTDLSource.from_url(url, loop=bot.loop)
-                voice_client.play(discord.FFmpegPCMAudio(source=file, **FFMPEG_OPTIONS))
-            
-            await ctx.message.add_reaction('▶️')
-            await ctx.send('**Now Playing:** {}'.format(title))
-            os.remove(file)
+        async with ctx.typing():
+            file, title = await AudioSource.from_url(url, loop=bot.loop)
+            voice_client.play(discord.FFmpegPCMAudio(source=file, **FFMPEG_OPTIONS))
+        
+        await ctx.message.add_reaction('▶️')
+        await ctx.send('**Now Playing:** {}'.format(title))
+        os.remove(file)
     
-        except:
-            await ctx.send("Vinyl is not connected to a voice channel currently")
+        #except:
+            #await ctx.send("Vinyl is not connected to a voice channel currently")
 
 
     @commands.command(name='pause', help='Pause any song Vinyl is currently playing')
